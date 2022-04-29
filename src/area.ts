b081b714e6c9adb8a4c609727ad3311d57b24fab
Mariano Carbajo <104107401+cobacho666@users.noreply.github.com>
2022-04-29 14:46:29 -0300

    Cambio de código Reescribí todo el código

--- src/area.ts
+++ src/area.ts
@@ -1,10 +1,11 @@
-let btnEnv = document.getElementById("btnEnviar");
+let btnCal = document.getElementById("btnCalcular");
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
 
-btnEnv.addEventListener("click", () => {
+btnCal.addEventListener("click", () => {
   let base: number = dato1.value;
   let altura: number = dato2.value;
   let area: number = base * altura;
-  console.log("El area es " + area);
+
+  console.log("El área es: ", area);
 });
